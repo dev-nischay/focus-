3cf1a1a176bd88e80fde5b59e5c61eb04522c516
dev-nischay <nischay.mehn@gmail.com>
2026-05-23 04:09:23 +0530

    updated ui component

--- ui/Card.tsx
+++ ui/Card.tsx
@@ -1,14 +1,31 @@
 import { minToHours } from "@/utils/timeCals";
 
-export const CardSmall = ({ mins, title, context }: { mins: number; title: string; context: string }) => {
-  const { hours, minutes } = minToHours(mins);
+export const CardSmall = ({
+  mins,
+  title,
+  context,
+  singleVal,
+}: {
+  mins?: number;
+  title: string;
+  context: string;
+  singleVal?: number | undefined;
+}) => {
+  function DurationValue({ mins }: { mins: number }) {
+    const { hours, minutes } = minToHours(mins);
+    return (
+      <span className="pl-2">
+        {hours}h {minutes}m
+      </span>
+    );
+  }
 
   return (
     <div className=" w-full h-fit bg-foreground p-4 border   shadow rounded-2xl border-customBorder  ">
       <div className="text-center flex flex-col gap-2 text-main">
         <div className="text-xs">{title}</div>
         <div className="text-2xl text-green-800 font-medium">
-          {hours}h <span className="pl-2">{minutes}m</span>
+          {singleVal ? <span>{singleVal}</span> : mins ? <DurationValue mins={mins} /> : <span></span>}
         </div>
         <div className="text-xs ">{context}</div>
       </div>
